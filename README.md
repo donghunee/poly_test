c4db4d86591ab53478586dd9e0bc4b7ae4180a5a
donghunee <ehdgns1766@naver.com>
2024-02-08 10:43:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## 프로젝트 실행 방법
+
+```shell
+$ git clone https://github.com/yoopark/wanted-pre-onboarding-frontend
+$ cd wanted-pre-onboarding-frontend/
+$ npm install
+$ npm start
+```
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+const locationModel = require("../model/location");
+
+/* GET home page. */
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Express" });
+});
+
+module.exports = router;
